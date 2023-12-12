ad0281c16438ecbdbdd6d8d5a469f78f0b3b247a
Andy Smith <asmith.esq@gmail.com>
2023-12-12 17:17:46 -0600

    Don't cache

--- helpers/apollo-client.js
+++ helpers/apollo-client.js
@@ -1,8 +1,13 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://www.office.tes.fm/api",
+  uri: process.env.NEXT_PUBLIC_CRAFT_ENDPOINT,
   cache: new InMemoryCache(),
+  defaultOptions: {
+    query: {
+      fetchPolicy: "no-cache",
+    },
+  },
 });
 
 export default client;
